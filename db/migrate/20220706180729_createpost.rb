e7154de041c7a8719c66210755d6f541fd2c8863
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-06 23:56:20 +0300

    Association added

--- db/migrate/20220706180729_createpost.rb
+++ db/migrate/20220706180729_createpost.rb
@@ -5,7 +5,7 @@ class Createpost < ActiveRecord::Migration[7.0]
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
-
+      t.references :author, foreign_key: { to_table: 'users' }
       t.timestamps
   end
 end
