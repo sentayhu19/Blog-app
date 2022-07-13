8eb23333cf04640e793d1665527e17156b3b436d
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-13 15:25:33 +0300

    Linters error fixed

--- app/controllers/post_controler.rb
+++ app/controllers/post_controler.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-def index
+  def index
     ###
-   end
- 
-   def show
-     ####
-   end
+  end
+
+  def show
+    ####
+  end
 end
--- app/controllers/users_controler.rb
+++ app/controllers/users_controler.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-def index
-   ###
+  def index
+    ###
   end
 
   def show
