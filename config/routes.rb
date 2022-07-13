217656d16b8a002d1699f8eae1621931a6d2cb34
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-13 15:51:51 +0300

    update on gitignore file and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
       #comments
       #likes
-    end
   end
 end
