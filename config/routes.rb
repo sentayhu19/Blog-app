5ff5a4c1d488e90063e6424163fe1ea3a39dca2c
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-17 00:20:00 +0300

    Like and comment routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-      #comments
-      #likes
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index new create show] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
   end
   root to: 'users#index'
 end
