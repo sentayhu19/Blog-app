8c67efe7c2a3848c720a98b73d50eac0afaae859
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-16 23:13:52 +0300

    Form for comment and like created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.includes(:user, comments: [:user]).find(params[:id])
   end
+  def create
+    post = params[:post]
+    user = User.find(params[:user_id])
+    new_post = Post.new(post.permit(:title, :text))
+    new_post.comments_counter = 0
+    new_post.likes_counter = 0
+    new_post.author_id = user.id
+
+    if new_post.save
+      flash[:notice] = 'New post created successfully.'
+      redirect_to user_post_url(user, new_post)
+    else
+      flash[:error] = 'Creating new post failed!'
+      @post = new_post
+      render :new
+    end
+  end
+  def new
+    @post = Post.new
+    render :new
+  end
 end
