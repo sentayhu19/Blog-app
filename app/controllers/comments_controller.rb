d7080d407fbb99584e27b6eb39c0853f9694f112
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-07-22 12:00:02 +0300

    Fix linters error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,6 +42,7 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
+
   def destroy
     @comment = Comment.find(params[:comment_id])
     @post = Post.find(@comment.post_id)
